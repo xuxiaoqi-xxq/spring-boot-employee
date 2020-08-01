a44d150d510752eadb0efcc36ad632bd39ecc874
xxq <xxq_work@163.com>
2020-08-02 00:15:31 +0800

    [employee service test][eva] feature: add test should_get_page_employees_when_get_by_page_given_page_pageSize

--- spring-boot-employee/src/test/java/com/oocl/service/EmployeeServiceTest.java
+++ spring-boot-employee/src/test/java/com/oocl/service/EmployeeServiceTest.java
@@ -4,6 +4,9 @@ import com.oocl.entity.Employee;
 import com.oocl.repository.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -27,5 +30,21 @@ public class EmployeeServiceTest {
 
         //then
         assertEquals(employees, foundEmployees);
+    }
+
+    @Test
+    void should_get_page_employees_when_get_by_page_given_page_pageSize() {
+        //given
+        EmployeeRepository employeeRepository = Mockito.mock(EmployeeRepository.class);
+        EmployeeService employeeService = new EmployeeService(employeeRepository);
+        Page<Employee> employees = new PageImpl<>(Arrays.asList(new Employee(1, "eva", "female", 18, 1000),
+                new Employee(2, "vae", "male", 20, 1000)));
+        given(employeeRepository.findAll(PageRequest.of(1, 2))).willReturn(employees);
+
+        //when
+        Page<Employee> employeesByPageAndPageSize = employeeService.findAllByPageAndPageSize(1, 2);
+
+        //then
+        assertEquals(employees, employeesByPageAndPageSize);
     }
 }
