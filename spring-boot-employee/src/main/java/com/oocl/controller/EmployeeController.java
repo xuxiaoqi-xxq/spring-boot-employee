026a19519dc01098a4854cc2a4e6b2ecc1e36c39
xxq <xxq_work@163.com>
2020-08-01 23:09:05 +0800

    [employee controller][eva] feature: get api /employees?page=.&pageSize=. will return range employees

--- spring-boot-employee/src/main/java/com/oocl/controller/EmployeeController.java
+++ spring-boot-employee/src/main/java/com/oocl/controller/EmployeeController.java
@@ -1,8 +1,8 @@
 package com.oocl.controller;
 
-import antlr.collections.AST;
 import com.oocl.entity.Employee;
 import com.oocl.service.EmployeeService;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +22,10 @@ public class EmployeeController {
     @GetMapping()
     public List<Employee> getAllEmployees() {
         return this.employeeService.findAll();
+    }
+
+    @GetMapping(params = {"page", "pageSize"})
+    public Page<Employee> getAllEmployeesByPageAndSize(Integer page, Integer pageSize) {
+        return this.employeeService.findAllByPageAndPageSize(--page, pageSize);
     }
 }
--- spring-boot-employee/src/main/java/com/oocl/service/EmployeeService.java
+++ spring-boot-employee/src/main/java/com/oocl/service/EmployeeService.java
@@ -1,6 +1,8 @@
 package com.oocl.service;
 
 import com.oocl.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -10,5 +12,9 @@ import java.util.List;
 public class EmployeeService {
     public List<Employee> findAll() {
         return Arrays.asList(new Employee());
+    }
+
+    public Page<Employee> findAllByPageAndPageSize(Integer integer, Integer pageSize) {
+        return new PageImpl<>(Arrays.asList(new Employee()));
     }
 }
