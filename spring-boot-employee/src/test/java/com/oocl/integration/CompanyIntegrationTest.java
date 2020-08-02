b9b2e5a6d0279f715f5027956de426c7035993bc
xxq <xxq_work@163.com>
2020-08-02 17:15:07 +0800

    [integration][eva] feature: test get /companies/{id}/employees will return employees of specific company

--- spring-boot-employee/src/test/java/com/oocl/integration/CompanyIntegrationTest.java
+++ spring-boot-employee/src/test/java/com/oocl/integration/CompanyIntegrationTest.java
@@ -1,14 +1,18 @@
 package com.oocl.integration;
 
 import com.oocl.entity.Company;
+import com.oocl.entity.Employee;
 import com.oocl.repository.CompanyRepository;
+import com.oocl.repository.EmployeeRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -25,6 +29,8 @@ public class CompanyIntegrationTest {
 
     @Autowired
     private CompanyRepository companyRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @AfterEach
     void tearDown() {
@@ -54,5 +60,24 @@ public class CompanyIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.companyId").isNumber())
                 .andExpect(jsonPath("$.name").value("oocl"));
+    }
+
+    @Test
+    void should_return_employee_when_hit_companies_endpoint_given_company_id() throws Exception {
+        //given
+        Company company = new Company(1, "oocw", null);
+        Company savedCompany = companyRepository.save(company);
+        List<Employee> employees = Arrays.asList(new Employee(1, "eva1", "female", 18, 1000),
+                new Employee(2, "eva2", "female", 19, 10000));
+
+        for (Employee employee: employees){
+            employee.setCompany(company);
+        }
+        employeeRepository.saveAll(employees);
+
+        mockMvc.perform(get("/companies/" + company.getCompanyId() + "/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].name").value("eva1"));
     }
 }
